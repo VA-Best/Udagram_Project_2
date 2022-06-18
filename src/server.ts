f9fc22e986a35ec73eb410f61a7821021012a7a4
VA-Best <adakolebest@gmail.com>
2022-06-18 22:43:19 +0100

    My first developemt branch; post review Stable code found in Main branch

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
 
-app.get("/filteredimage", async (req, res) => {
+app.get("/filteredimage", async (req:express.Request, res:express.Response) => {
   const imageUrl : string = req.query.image_url;
 
   if (!imageUrl) {
